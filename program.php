ab5cf11992f9ebd94f4cdf050dc9e587e6c942e4
LukasRalys <lukasralys10@gmail.com>
2023-06-08 16:00:07 +0300

    first commit

---
+++ program.php
@@ -0,0 +1,22 @@
+<?php
+include 'class.php';
+include 'duktclass.php';
+    //0,1,1,2,3,5,..
+$fn=10**7;
+$obj=new duktseq($fn);
+
+$obj-> fibionaci ();
+
+//print ($obj-> ans1. '<br>');
+print ($obj-> get_answer() . '<br>');
+//print ("<br>");
+$obj-> setdata (500);
+$obj-> fibionaci ();
+print ($obj-> get_answer() . '<br>');
+//print ($obj-> get_n());
+$obj-> setdata (500);
+$obj-> arit (2, 3);
+print ($obj-> get_answer() . '<br>');
+
+
+?>
